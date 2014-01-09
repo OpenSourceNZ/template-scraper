b786d7016dbc6d38e37646324f920acbf3f6c38e
Kefeng Deng <dkf_genius@hotmail.com>
2014-01-09 15:47:28 +1300

    ready

--- src/main/groovy/nz/net/osnz/common/scraper/TemplateInterpreter.groovy
+++ src/main/groovy/nz/net/osnz/common/scraper/TemplateInterpreter.groovy
@@ -7,177 +7,180 @@ import org.springframework.util.StringUtils
 
 class TemplateInterpreter {
 
-  /**
-   * The parsed document
-   */
-  private static Map<ScraperLayout, Document> parsedDoc = [:];
+    /**
+     * The parsed document
+     */
+    private static Map<ScraperLayout, Document> parsedDoc = new HashMap<>();
 
-  /**
-   * Layout configuration
-   */
-  private ScraperLayout layout;
+    /**
+     * Layout configuration
+     */
+    private ScraperLayout layout;
 
-  /**
-   * Layout information
-   *
-   * @param layout is the layout to use
-   */
-  public TemplateInterpreter(ScraperLayout layout) {
-    if (layout == null) {
-      throw new IllegalArgumentException("Not a valid layout instance");
-    }
-    this.layout = layout;
-  }
-
-
-  /**
-   * Retrieve the template content
-   */
-  public Document getTemplateContent(boolean doRefresh = false) {
-
-    if (!parsedDoc[layout] || doRefresh) {
-
-      Document doc = getTemplateDocument()
-
-      if (doc) {
-
-        doc.select("html")
-
-        // remove the title
-        doc.select("head title").remove()
-
-        // remove the analytics block
-        removeAnalyticsScriptBlock(doc)
-
-        // remove resources from header
-        removeIgnoredIncludes(doc, layout.filter)
-
-        // sanitize stylesheet urls
-        doc.select("[href]").each { node -> rebaseAssetForNode(node, "href") }
-        doc.select("[src]").each { node -> rebaseAssetForNode(node, "src") }
-
-        parsedDoc[layout] = doc
-      }
+    /**
+     * Layout information
+     *
+     * @param layout is the layout to use
+     */
+    public TemplateInterpreter(ScraperLayout layout) {
+        if (layout == null) {
+            throw new IllegalArgumentException("Not a valid layout instance");
+        }
+        this.layout = layout;
     }
 
-    return parsedDoc[layout]
 
-  }
+    /**
+     * Retrieve the template content
+     */
+    public Document getTemplateContent(boolean doRefresh=false) {
 
-  /**
-   * Retrieve the <head /> content
-   * @return
-   */
-  public String getHtmlHead() {
-    return this.getTemplateContent().select("head").html();
-  }
+        if (!parsedDoc[layout] || doRefresh) {
 
-  /**
-   * Get the university body
-   *
-   * @param leftBar show the left
-   * @param rightBar
-   * @param content
-   * @return
-   */
-  public String getBodyWithContent(String container, String content, boolean leftBar = false, String leftBarContainerId = 'leftBar', boolean rightBar = false, String rightBarContainerId = 'rightBar') {
-    Document freshDoc = this.getTemplateContent().clone()
+            Document doc = getTemplateDocument()
 
-    if (!leftBar && leftBarContainerId) {
-      freshDoc.select("#${leftBarContainerId}")?.remove()
+            if (doc) {
+
+                doc.select("html")
+
+                // remove the title
+                doc.select("head title").remove()
+
+                // remove the analytics block
+                removeAnalyticsScriptBlock(doc)
+
+                // remove resources from header
+                removeIgnoredIncludes(doc, layout.filter)
+
+                // sanitize stylesheet urls
+                doc.select("[href]").each {
+                    node -> rebaseAssetForNode(node, "href")
+                }
+                doc.select("[src]").each {
+                    node -> rebaseAssetForNode(node, "src")
+                }
+
+                parsedDoc[layout] = doc
+            }
+        }
+
+        return parsedDoc[layout]
+
     }
 
-    if (!rightBar && rightBarContainerId ) {
-      freshDoc.select("#${rightBarContainerId}")?.remove()
+    /**
+     * Retrieve the <head /> content
+     *
+     * @return
+     */
+    public String getHtmlHead() {
+        return this.getTemplateContent().select("head").html();
     }
 
-    if ( !StringUtils.isEmpty(this.layout.container) ) {
-      container = this.layout.container
+    /**
+     * Get the university body
+     *
+     * @param leftBar  show the left
+     * @param rightBar
+     * @param content
+     * @return
+     */
+    public String getBodyWithContent(String container, String content, boolean leftBar=false, String leftBarContainerId='leftBar', boolean rightBar=false, String rightBarContainerId='rightBar') {
+        Document freshDoc = this.getTemplateContent().clone()
+
+        if (!leftBar && leftBarContainerId) {
+            freshDoc.select("#${leftBarContainerId}") ?.remove()
+        }
+
+        if (!rightBar && rightBarContainerId) {
+            freshDoc.select("#${rightBarContainerId}") ?.remove()
+        }
+
+        if (!StringUtils.isEmpty(this.layout.container)) {
+            container = this.layout.container
+        }
+
+        freshDoc.select("#${container}").html(content)
+
+        return freshDoc.select("body").html()
     }
 
-    freshDoc.select("#${container}").html(content)
-
-    return freshDoc.select("body").html()
-  }
-
-  /**
-   * Remove a number of script includes from the header document to take
-   * back control over which javascript libraries are included.
-   *
-   * @param uri is a list of uris to remove
-   */
-  protected void removeIgnoredIncludes(Document doc, String... filters) {
-    removeIgnoredIncludes(doc, filters)
-  }
-
-  protected void removeIgnoredIncludes(Document doc, List<String> filters) {
-    filters?.each { String location ->
-      doc.select("script").findAll {Element element ->
-        element.attr('src').toLowerCase().trim().find( location.trim() ) != null
-      }*.remove()
+    /**
+     * Remove a number of script includes from the header document to take
+     * back control over which javascript libraries are included.
+     *
+     * @param uri is a list of uris to remove
+     */
+    protected void removeIgnoredIncludes(Document doc, String... filters) {
+        removeIgnoredIncludes(doc, filters);
     }
-  }
 
-  /**
-   * Run closure on nodes that have analytics information
-   *
-   * @param doc is the jsoup document to go through
-   * @param closure is the closure to invoke
-   */
-  protected void removeAnalyticsScriptBlock(Document doc) {
-    doc.select("script").each { Element node ->
-      if (hasAnalyticsInformation(node)) {
-        node.remove()
-      }
+    protected void removeIgnoredIncludes(Document doc, List<String> filters) {
+        filters ?.each {
+            String location->
+                doc.select("script").findAll {
+                    Element element->
+                        element.attr('src').toLowerCase().trim().find(location.trim()) != null
+                }*.remove()
+        }
     }
-  }
 
-  /**
-   * Analytics information?
-   *
-   * @param node
-   * @return
-   */
-  protected boolean hasAnalyticsInformation(Element node) {
-    node.html().contains("_setAccount")
-  }
-
-
-  /**
-   * Sanitize the node
-   *
-   * @param node node to sanitize
-   * @param attr attribute to query
-   */
-  protected void rebaseAssetForNode(node, attr) {
-    def url = node.attr(attr)
-    if (url.indexOf("://") == -1) {
-      node.attr(attr, this.getRelativeLinksBase() + url)
+    /**
+     * Run closure on nodes that have analytics information
+     *
+     * @param doc     is the jsoup document to go through
+     */
+    protected void removeAnalyticsScriptBlock(Document doc) {
+        for ( Element node : doc.select("script") ) {
+            if (hasAnalyticsInformation(node)) {
+                node.remove();
+            }
+        }
     }
-  }
 
-  /**
-   * @return the document parsed from the university's page.
-   */
-  protected Document getTemplateDocument() {
-    return Jsoup.connect(getTemplateBaseLocation()).get()
-  }
-
-  /**
-   * @return the base location for the static files
-   */
-  protected String getRelativeLinksBase() {
-    return layout.assets
-  }
-
-  /**
-   * @return configured template location
-   */
-  protected String getTemplateBaseLocation() {
-    return layout.url
-  }
+    /**
+     * Analytics information?
+     *
+     * @param node
+     * @return
+     */
+    protected boolean hasAnalyticsInformation(Element node) {
+        return node.html().contains("_setAccount");
+    }
 
 
+    /**
+     * Sanitize the node
+     *
+     * @param node node to sanitize
+     * @param attr attribute to query
+     */
+    protected void rebaseAssetForNode(org.jsoup.nodes.Node node, String attr) {
+        String url = node.attr(attr);
+        if ( !url.contains(":") ) {
+            node.attr(attr, this.getRelativeLinksBase() + url);
+        }
+    }
 
+    /**
+     * @return the document parsed from the university's page.
+     */
+    protected Document getTemplateDocument() throws IOException {
+        return Jsoup.connect(getTemplateBaseLocation()).get();
+    }
+
+    /**
+     * @return the base location for the static files
+     */
+    protected String getRelativeLinksBase() {
+        return layout.getAssets();
+    }
+
+    /**
+     * @return configured template location
+     */
+    protected String getTemplateBaseLocation() {
+        return layout.getUrl();
+    }
 
 }
